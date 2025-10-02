90ee52263ea26aebea96cdf6e17b6fe4d086ea80
NealSmalley <neal.e.smalley@gmail.com>
2025-10-02 10:29:27 -0600

    creation of the copy constructor for ChessPieces for chessBoard

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -14,6 +14,11 @@ public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
 
+    //deep copy piece
+    public ChessPiece copy(){
+        return new ChessPiece(pieceColor, type);
+    }
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;

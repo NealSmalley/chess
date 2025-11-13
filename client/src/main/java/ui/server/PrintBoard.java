1d1d03fa3d23c1abbfb1af622622a8e4e1c6b276
NealSmalley <neal.e.smalley@gmail.com>
2025-11-12 17:22:47 -0700

    full first draft of PrintBoard class

--- client/src/main/java/ui/server/PrintBoard.java
+++ client/src/main/java/ui/server/PrintBoard.java
@@ -1,7 +1,7 @@
 package ui.server;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Stack;
 
 public class PrintBoard {
@@ -12,19 +12,10 @@ public class PrintBoard {
     Stack<String> firstRowLetters = new Stack<>();
     //
     Stack<String> secondRowColors = new Stack<>();
-    public enum TopBoarder{
-        a,
-        b,
-        c,
-        d,
-        e,
-        f,
-        g,
-        h;
-    }
-    List<String> letters = List.of("R","N","B","Q","K","B","N","R");
+    public enum TopBoarder{a, b, c, d, e, f, g, h;}
+    List<String> lettersOutside = List.of("R","N","B","Q","K","B","N","R");
 
-    //color
+    //color based vars
     private String playerColor;
     private int start;
     private int end;
@@ -34,75 +25,98 @@ public class PrintBoard {
     private int letterNumber;
     private int letterIncrementer;
 
-    //effected by color
-    if (white){
-        start = 0;
-        end = TopBoarder.values().length;
-        incrementer = 1;
-        sideNumber = 8;
-        sideIncrementer = -1;
-        letterNumber = 0;
-        letterIncrementer = 1;
-    }
-    else {
-        start = TopBoarder.values().length - 1;
-        end = -1;
-        incrementer = -1;
-        sideNumber = 0;
-        sideIncrementer = 1;
-        letterNumber = 8;
-        letterIncrementer = -1;
-    }
+    //row based vars
+    private Stack<String> rowPopStack;
+    private Stack<String> rowPushStack;
+    private int rowCount;
 
+    public void playerColorVars(String playerColor){
+        //effected by color
+        if (Objects.equals(playerColor, "white")){
+            start = 0;
+            end = TopBoarder.values().length;
+            incrementer = 1;
+            sideNumber = 8;
+            sideIncrementer = -1;
+            letterNumber = 0;
+            letterIncrementer = 1;
+        }
+        else {
+            start = TopBoarder.values().length - 1;
+            end = -1;
+            incrementer = -1;
+            sideNumber = 0;
+            sideIncrementer = 1;
+            letterNumber = 8;
+            letterIncrementer = -1;
+        }
+    }
     public void printBoard(String playerColor){
-        switch(playerColor){
-            case "white" ->
-            case "black" ->
-        }
+        playerColorVars(playerColor);
         String currentCol = colStack.pop();
-        switch(currentCol) {
-            case "boarder" -> boarder();
-            case "1st" -> firstrow();
-            case "2nd" -> secondrow();
+        if (currentCol.equals("boarder")){
+            boarder();
         }
-
-        //incrementer (if called twice)
-        //fill first stack()
-        //pop off the first stack
-        //push to the second stack
-        //black
-            //letter row()
+        else if (currentCol.equals("1st")){
+            rowPopStack = firstRowColors;
+            rowPushStack = secondRowColors;
+            row(rowPopStack, rowPushStack);
+        }
+        else if (currentCol.equals("2nd")){
+            rowPopStack = secondRowColors;
+            rowPushStack = firstRowColors;
+            row(rowPopStack, rowPushStack);
+        }
     }
-
     public void boarder(){
         //forward = white/reverse = black
         for (int i = start; i != end; i = i+incrementer){
             System.out.println(TopBoarder.values()[i]);
         }
     }
-    public void firstrow(){
-        String square = firstRowColors.pop();
+    public void row(Stack<String> rowPopStack,Stack<String> rowPushStack){
+        rowCount++;
+        //side boards
+        String square = rowPopStack.pop();
+        rowPushStack.push(square);
         System.out.println(square);
-        //if square == boarder
-            sideBoarder();
-        //if square == white || black
-            //firstRowChess()
-        secondRowColors.push(square);
+
+        perimeterOrArea(rowPopStack,rowPushStack,square);
     }
     public void sideBoarder(){
         System.out.println(sideNumber);
         sideNumber = sideNumber + sideIncrementer;
     }
-    public void firstRowChess(){
-        String square = firstRowColors.pop();
+    public void RowChess(Stack<String> rowPopStack, Stack<String> rowPushStack){
+        String square = rowPopStack.pop();
         //color squares
         System.out.println(square);
-        secondRowColors.push(square);
+        rowPushStack.push(square);
         //letters
-        String letter = letters.get(letterNumber);
-        System.out.println(letter);
-        letterNumber = letterNumber + letterIncrementer;
+        letters();
+    }
+    private void perimeterOrArea(Stack<String> rowPopStack, Stack<String> rowPushStack, String square){
+        //side boarder
+        if (Objects.equals(square, "boarder")) {
+            sideBoarder();
+        }
+        //inner area row
+        if ((Objects.equals(square, "white")) || (Objects.equals(square, "black"))) {
+            RowChess(rowPopStack, rowPushStack);
+        }
+    }
 
+    private void letters(){
+        //lettersOuter
+        if ((rowCount == 2)||(rowCount == 9)) {
+            String letter = lettersOutside.get(letterNumber);
+            System.out.println(letter);
+        }
+        //lettersInner
+        else if((rowCount == 3)||(rowCount == 8)) {
+            System.out.println("P");
+        }
+        letterNumber = letterNumber + letterIncrementer;
     }
 
 }
